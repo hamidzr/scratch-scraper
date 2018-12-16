6795df4927f63252e5cb5d14c45d2398615afba3
Hamid Zare <dellydela@gmail.com>
2018-12-16 15:37:57 -0600

    debug logging module

---
+++ src/logger.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const levels = ['trace', 'info', 'log', 'debug', 'warn', 'error'],
+    debug = require('debug');
+
+// set which debug levels to send to stderr, the rest will go to stdout
+const STDERR = ['warn', 'error'];
+
+const Logger = function(name) {
+    this._name = name;
+    /* eslint-disable no-console*/
+    levels.forEach(lvl => {
+        this[lvl] = debug(`${name}:${lvl}`);
+        if (STDERR.find(item => item === lvl)) {
+            this[lvl].log = console.error.bind(console);
+        } else { // send to stdout
+            this[lvl].log = console.log.bind(console);
+        }
+    });
+    /* eslint-enable no-console*/
+};
+
+Logger.prototype.fork = function(name) {
+    return new Logger([this._name, name].join(':'));
+};
+
+const logger = new Logger('scraper');
+
+module.exports = logger;
