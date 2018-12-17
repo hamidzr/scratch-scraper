52bc7e7bd66ae175e1d92a7f28d61e1834aa6faa
Hamid Zare <dellydela@gmail.com>
2018-12-16 21:06:43 -0600

    wrote a crawler class

--- src/crawler.js
+++ src/crawler.js
@@ -1,8 +1,8 @@
 // const osmosis = require('osmosis');
 const fs = require('fs');
 const logger = require('./logger').fork('crawler');
-const { sleep } = require('./utils');
-const { usersFromComments } = require('./scratch/users');
+const { sleep, load } = require('./utils');
+const { parseUsersFromComments, userCommentsUrl } = require('./scratch/users');
 
 
 /*
@@ -17,73 +17,121 @@ projectId:
   comments: users
 */
 
-const userStore = new Set();
-let  usernamesQ = []
+class Crawler {
+  constructor(opts) {
+    const defaultOpts = {
+      reqBatch: 20,
+      reqDelay: 300,
+      logger: logger,
+      // picksup the interesting values from the page and also returns the newly found links
+      pageParser: undefined // might have a better place..
+    };
+    opts = {...defaultOpts, ...opts};
 
-const memory = {};
+    if (!opts.pageParser) throw new Error('pageParser is required');
 
-function isCrawled(type, variable) {
-  if (memory[type] === undefined) {
-    memory[type] = {};
+    this.pageParser = opts.pageParser;
+    this.reqBatch = opts.reqBatch;
+    this.reqDelay = opts.reqDelay;
+    this.logger = logger;
+
+    this._queue = []; // queue of discovered urls
+    this._visited = {}; // keep track of visited urls
+    this.crawledPages = 0;
   }
-  if (memory[type][variable] === true) {
-    return true;
+
+  isCrawled(url) {
+    return this._visited[url] === true;
   }
-  return false
-}
 
-function markCrawled(type, variable) {
-  memory[type][variable] = true;
-}
+  markCrawled(url) {
+    this._visited[url] = true;
+    this.crawledPages = this.crawledPages + 1;
+  }
 
+  _queueNewUrls(newUrls) {
+    newUrls = newUrls.filter(url => !this.isCrawled(url));
+    this._queue.push(...newUrls);
+  }
 
-async function consume(biteSize) {
-  let chunk = usernamesQ.splice(0, biteSize);
-
-  chunk = chunk.filter(username => !isCrawled('comments', username));
-  // if (chunk.length < biteSize * 0.7) chunk = chunk.push(...usernamesQ.splice(0, biteSize));
-
-  let promises = chunk.map(async username => {
-    markCrawled('comments', username);
-    try {
-      let nextUsers = await usersFromComments(username);
-      userStore.add(username);
-      usernamesQ.push(...nextUsers);
-    } catch (e) {
-      logger.error('error scraping comments from', username);
+  // picks atleast "count" urls from the head of the queue
+  // best effort. changes the queue
+  // OPT double checking for isCrawled
+  _pickNewUrls(count) {
+    let newUrls = this._queue.splice(0, count);
+    newUrls = newUrls.filter(url => !this.isCrawled(url));
+    while (newUrls.length < count && this._queue.length > count) {
+      let url = this._queue.shift();
+      if (!this.isCrawled(url)) newUrls.add(url); // OPT O(n) on every call
     }
-  })
-  // res = await Promise.all(promises);
+    return newUrls;
+  }
+
+  // carwls a batch
+  async crawl() {
+    let urls = this._pickNewUrls(this.reqBatch);
+    urls.forEach(async url => {
+      try {
+        this.markCrawled(); // not actually crawled yet..
+        let resp = await load(url);
+        const newUrls = this.pageParser(resp); // maybe return the values too?
+        this._queueNewUrls(newUrls);
+      } catch (e) {
+        this.logger.error('error crawling', url);
+      }
+    })
+  }
+
+
+  _removeDuplicates(arr) {
+    let initLen = arr.length;
+    let newArr =  Array.from(new Set(arr));
+    return newArr;
+    // return [...new Set(arr)];
+  }
+
+  _cleanupQueue() {
+    this._queue = this._removeDuplicates(this._queue);
+  }
+
+
+
+  async start(seedUrls) {
+    this._queueNewUrls(seedUrls);
+
+    // not the best way to cleanup..
+    setInterval(() => {
+      this._cleanupQueue();
+    }, 3000)
+
+    setInterval(() => {
+      fs.writeFile(`queue-${Date.now()}.list`, this._queue.join('\n'), () => {});
+      fs.writeFile(`visited-${Date.now()}.list`, Object.keys(this._visited).join('\n'), () => {});
+    }, 30000)
+
+    while (true) {
+      this.crawl();
+      await sleep(this.reqDelay)
+      logger.log(`total visited: ${this.crawledPages} in queue: ${this._queue.length}`);
+    }
+
+  }
+
+}
+
+const userStore = new Set();
+
+const pageParser = function(dom) {
+  let users = parseUsersFromComments(dom);
+  users.forEach(u => userStore.add(u));
+  let newUrls = users.map(u => userCommentsUrl(u));
+  return newUrls;
 };
 
+const crawler = new Crawler({pageParser})
+const startUrls = ['ScratchCat'].map(user => userCommentsUrl(user));
 
-function removeDuplicates(arr) {
-  let initLen = arr.length;
-  let newArr =  Array.from(new Set(arr));
-  logger.trace(`saved ${initLen - newArr.length}`);
-  return newArr;
-  // return [...new Set(arr)];
-}
+crawler.start(startUrls);
 
-async function start() {
-  usernamesQ.push('ScratchCat');
-
-  setInterval(() => {
-    usernamesQ = removeDuplicates(usernamesQ);
-  }, 3000)
-
-  setInterval(() => {
-    fs.writeFile(`usernamesQ-${Date.now()}.list`, usernamesQ.join('\n'), () => logger.trace('finished writing'));
-    fs.writeFile(`testedUsers-${Date.now()}.list`, Array.from(userStore).join('\n'), () => logger.trace('finished writing'));
-  }, 30000)
-
-  while (true) {
-    consume(10);
-    await sleep(300)
-    logger.log('totalusers:', userStore.size);
-    logger.log('queue:', usernamesQ.length);
-  }
-
-}
-
-start();
+// write to disk every once in a while
+// TODO write a persistance strategy to appends newly found users to disk
